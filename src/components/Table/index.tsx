5120d529d5eb56ff7ed2dbe9a8ced786db833f04
ochom <ochomrichard752@gmail.com>
2023-02-28 10:00:06 +0300

    auto-version

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -170,11 +170,11 @@ export default function Table({
         <Pagination>
           <span>Rows per page</span>
           <select value={rowsPerPage} onChange={handleChangeRowsPerPage}>
-            <option value={3}>3</option>
-            <option value={5}>5</option>
-            <option value={10}>10</option>
-            <option value={25}>25</option>
-            <option value={50}>50</option>
+            {paginationRowsPerPageOptions.map((option: number) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
           </select>
           <button onClick={goToFirstPage} disabled={page === 0}>
             <KeyboardDoubleArrowLeft />
