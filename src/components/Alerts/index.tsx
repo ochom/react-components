e0b3961bbb05e08268c27fcd7d4443f231642ada
Richard Ochom <ochomrichard752@gmail.com>
2023-04-08 21:08:55 +0300

    update alerts usage

--- src/components/Alerts/index.tsx
+++ src/components/Alerts/index.tsx
@@ -1,16 +1,120 @@
 import "./styles.css";
-import { useContext } from "react";
-import { SnackContext } from "./snack";
-import { ConfirmDialogContext } from "./confirm";
+import ConfirmModal from "./confirm";
+import { ConfirmProps, SnackProps } from "./props";
+import { Alert, Slide, Snackbar, SnackbarOrigin } from "@mui/material";
+import React, { createContext, useContext, useState } from "react";
+import SnackAlert from "./snack";
 
+const initConfirmState: ConfirmProps = {
+  open: false,
+  title: "",
+  message: "",
+  showCancelButton: true,
+  confirmButtonColor: "error",
+  cancelButtonColor: "success",
+  confirmButtonText: "Yes, Delete",
+  cancelButtonText: "No, Cancel",
+  onConfirm: () => {},
+  onCancel: () => {},
+};
 
-export  {PageConstruction, ErrorPage} from "./pages";
+const initSnackState: SnackProps = {
+  type: "success",
+  open: false,
+  message: "",
+  duration: 3000,
+  origin: { vertical: "bottom", horizontal: "right" },
+  children: null,
+};
+
+const AlertContext = createContext({
+  setConfirmState: (options: ConfirmProps) => {},
+  alertSuccess: (message: string) => {},
+  alertError: (message: string) => {},
+  alertWarning: (message: string) => {},
+});
+
+export const AlertProvider = ({ children }: any) => {
+  const [confirmState, setConfirmState] = useState(initConfirmState);
+  const [snackState, setSnackState] = useState(initSnackState);
+
+  const handleCloseConfirm = () => {
+    setConfirmState(initConfirmState);
+  };
+
+  const handleConfirm = () => {
+    if (confirmState.onConfirm) confirmState.onConfirm();
+    handleCloseConfirm();
+  };
+
+  const handleCancel = () => {
+    if (confirmState.onCancel) confirmState.onCancel();
+    handleCloseConfirm();
+  };
+
+  const confirmButtons = [
+    {
+      text: confirmState.confirmButtonText,
+      color: confirmState.confirmButtonColor,
+      onClick: handleConfirm,
+      hidden: false,
+    },
+    {
+      text: confirmState.cancelButtonText,
+      color: confirmState.cancelButtonColor,
+      onClick: handleCancel,
+      hidden: !confirmState.showCancelButton,
+    },
+  ];
+
+  const createSnack = (
+    message: string,
+    type: "success" | "error" | "warning"
+  ) => {
+    setSnackState({
+      ...snackState,
+      message,
+      type,
+      open: true,
+    });
+  };
+
+  const closeSnack = () => setSnackState({ ...snackState, open: false });
+
+  const alertSuccess = (message: string) => {
+    createSnack(message, "success");
+  };
+
+  const alertError = (message: string) => {
+    createSnack(message, "error");
+  };
+
+  const alertWarning = (message: string) => {
+    createSnack(message, "warning");
+  };
+
+  const providerProps = {
+    setConfirmState,
+    alertSuccess,
+    alertError,
+    alertWarning,
+  };
+
+  return (
+    <AlertContext.Provider value={providerProps}>
+      {children}
+      <ConfirmModal
+        data={confirmState}
+        handleClose={handleCloseConfirm}
+        buttons={confirmButtons}
+      />
+      <SnackAlert data={snackState} closeSnack={closeSnack} />
+    </AlertContext.Provider>
+  );
+};
 
 export const useAlerts = () => {
-  const {snackSuccess, snackError, snackWarning, closeSnack } = useContext(SnackContext)
-  const {confirm} = useContext(ConfirmDialogContext)
-  return {snackSuccess, snackError, snackWarning, closeSnack, confirm}
-}
-
-export { SnackProvider } from "./snack";
-export { ConfirmDialogProvider } from './confirm';
+  const { alertSuccess, alertError, alertWarning, setConfirmState: confirm } =
+    useContext(AlertContext);
+  return { alertSuccess, alertError, alertWarning, confirm };
+};
--- src/components/Alerts/snack.tsx
+++ src/components/Alerts/snack.tsx
@@ -1,79 +1,26 @@
 import { Alert, Slide, Snackbar, SnackbarOrigin } from "@mui/material";
 import React, { createContext, useContext, useState } from "react";
+import { SnackAlertProps } from "./props";
 
 const TransitionUp = (props: any) => {
   return <Slide {...props} direction="up" />;
 };
 
-export const SnackContext = createContext({
-  snackSuccess: (message: string) => {},
-  snackError: (message: string) => {},
-  snackWarning: (message: string) => {},
-  closeSnack: () => {},
-});
-
-export interface SnackProviderProps {
-  duration?: number;
-  origin?: SnackbarOrigin;
-  children: any;
+function SnackAlert({ data, closeSnack }: SnackAlertProps) {
+  const { type, open, message, duration, origin } = data;
+  return (
+    <Snackbar
+      open={open}
+      anchorOrigin={origin}
+      autoHideDuration={duration}
+      onClose={() => closeSnack()}
+      TransitionComponent={TransitionUp}
+    >
+      <Alert severity={type} sx={{ width: "100%" }}>
+        {message}
+      </Alert>
+    </Snackbar>
+  );
 }
 
-export const SnackProvider = ({
-  duration = 3000,
-  origin = { vertical: "bottom", horizontal: "right" },
-  children,
-}: SnackProviderProps) => {
-  const [open, setOpen] = useState(false);
-  const [message, setMessage] = useState("");
-  const [type, setType] = useState<"success" | "error" | "warning">("success");
-
-  const createSnack = (
-    message: string,
-    type: "success" | "error" | "warning"
-  ) => {
-    setMessage(message);
-    setType(type);
-    setOpen(true);
-  };
-
-  const closeSnack = () => {
-    setOpen(false);
-  };
-
-  const snackSuccess = (message: string) => {
-    createSnack(message, "success");
-  };
-
-  const snackError = (message: string) => {
-    createSnack(message, "error");
-  };
-
-  const snackWarning = (message: string) => {
-    createSnack(message, "warning");
-  };
-
-  const providerProps = {
-    snackSuccess,
-    snackError,
-    snackWarning,
-    closeSnack,
-  };
-
-  return (
-    <SnackContext.Provider value={providerProps}>
-      {children}
-      <Snackbar
-        open={open}
-        anchorOrigin={origin}
-        autoHideDuration={duration}
-        onClose={() => closeSnack()}
-        TransitionComponent={TransitionUp}
-      >
-        <Alert severity={type} sx={{ width: "100%" }}>
-          {message}
-        </Alert>
-      </Snackbar>
-    </SnackContext.Provider>
-  );
-};
-
+export default SnackAlert;
