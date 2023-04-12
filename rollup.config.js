f54b7c678c2c06c86d69300f65ab741192dcf586
Richard Ochom <ochomrichard752@gmail.com>
2023-04-12 10:12:15 +0300

    fix rollup

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import babel from "@rollup/plugin-babel";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import postcss from "rollup-plugin-postcss";
 import resolve from "@rollup/plugin-node-resolve";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 
 export default [
