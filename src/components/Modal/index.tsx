01d0a578c1e8aeffe14cb39b130dcad48d4cdc34
Richard Ochom <ochomrichard752@gmail.com>
2023-05-15 16:01:11 +0300

    auto-version

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -140,7 +140,7 @@ export const Modal = ({
   useEffect(() => {
     const handleEscape = (event: any) => {
       if (event.key === "Escape" && isOpen) {
-        handleClose();
+        event.preventDefault();
       }
     };
     window.addEventListener("keydown", handleEscape);
