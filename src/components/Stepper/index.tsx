efa2247804699d7c2b88a4f5f6fafa99fb0f2f67
Richard Ochom <ochomrichard752@gmail.com>
2023-09-26 13:45:04 +0300

    feat: add stepper orientation

--- src/components/Stepper/index.tsx
+++ src/components/Stepper/index.tsx
@@ -2,6 +2,7 @@ import {
   Box,
   Container,
   Step,
+  StepContent,
   StepIcon,
   StepLabel,
   Stepper,
@@ -27,41 +28,39 @@ export default function CustomStepper({
   orientation = "horizontal",
 }: StepperProps) {
   return (
-    <Container>
-      <Stepper activeStep={activeStep} orientation={orientation}>
-        {steps.map(({ title, icon }, index) => {
-          return (
-            <Step
-              key={index}
-              sx={{
-                "& .MuiStepLabel-root .Mui-completed": {
-                  color: "primary.main", // circle color (COMPLETED)
+    <Stepper activeStep={activeStep} orientation={orientation}>
+      {steps.map(({ title, icon }, index) => {
+        return (
+          <Step
+            key={index}
+            sx={{
+              "& .MuiStepLabel-root .Mui-completed": {
+                color: "primary.main", // circle color (COMPLETED)
+              },
+              "& .MuiStepLabel-label.Mui-completed.MuiStepLabel-alternativeLabel":
+                {
+                  color: "grey.500", // Just text label (COMPLETED)
                 },
-                "& .MuiStepLabel-label.Mui-completed.MuiStepLabel-alternativeLabel":
-                  {
-                    color: "grey.500", // Just text label (COMPLETED)
-                  },
-                "& .MuiStepLabel-root .Mui-active": {
-                  color: "primary.main", // circle color (ACTIVE)
+              "& .MuiStepLabel-root .Mui-active": {
+                color: "primary.main", // circle color (ACTIVE)
+              },
+              "& .MuiStepLabel-label.Mui-active.MuiStepLabel-alternativeLabel":
+                {
+                  color: "common.white", // Just text label (ACTIVE)
                 },
-                "& .MuiStepLabel-label.Mui-active.MuiStepLabel-alternativeLabel":
-                  {
-                    color: "common.white", // Just text label (ACTIVE)
-                  },
-                "& .MuiStepLabel-root .Mui-active .MuiStepIcon-text": {
-                  fill: "common.white", // circle's number (ACTIVE)
-                },
-              }}
-            >
-              {icon && <StepIcon icon={icon} />}
-              <StepLabel>{title}</StepLabel>
-            </Step>
-          );
-        })}
-      </Stepper>
-      <Box>
-        <React.Fragment>{steps[activeStep].content}</React.Fragment>
-      </Box>
-    </Container>
+              "& .MuiStepLabel-root .Mui-active .MuiStepIcon-text": {
+                fill: "common.white", // circle's number (ACTIVE)
+              },
+            }}
+          >
+            {icon && <StepIcon icon={icon} />}
+            <StepLabel>{title}</StepLabel>
+            <StepContent>
+              <Box>{steps[activeStep].content}</Box>
+            </StepContent>
+          </Step>
+        );
+      })}
+    </Stepper>
   );
 }
