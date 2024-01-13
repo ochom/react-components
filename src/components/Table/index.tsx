451e3c2d8297ad5c220845cb908a4bbe4937fe01
ochom <ochomrichard752@gmail.com>
2024-01-13 23:17:33 +0300

    adjust page number

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -43,7 +43,7 @@ export default function Table({
 
   useEffect(() => {
     if (serverSide && onPaginationChange) {
-      onPaginationChange(page, rowsPerPage);
+      onPaginationChange(page + 1, rowsPerPage);
     }
   }, [page, rowsPerPage]);
 
