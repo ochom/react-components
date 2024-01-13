ae9fcb9693f10b06b01fa87857fa1e0103580885
ochom <ochomrichard752@gmail.com>
2024-01-14 00:05:36 +0300

    fix: handle refetch in library

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -96,6 +96,7 @@ export default function Table({
           loading={loading}
           error={error}
           message={emptyMessage}
+          serverSide={serverSide}
           cols={cols}
           rows={rows}
           rowsPerPage={rowsPerPage}
