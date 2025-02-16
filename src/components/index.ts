da2359f2d3d590fe4ba854c41e251c011df2be24
Richard Ochom <ochomrichard752@gmail.com>
2025-02-16 14:18:33 +0300

    remove material alerts

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,8 +3,7 @@ export { default as Modal } from "./Modal";
 export { default as Stepper } from "./Stepper";
 export { default as Table } from "./Table";
 export { default as Tabs } from "./Tabs";
-
-export * from "./Alerts";
+ 
 export * from "./Drawer/drawer";
 export * from "./EmptyPage";
 export * from "./Form";
