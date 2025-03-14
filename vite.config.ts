b7c8a2420ad677518ba593ab242a87d4de0ddb55
Richard Ochom <ochomrichard752@gmail.com>
2025-03-14 09:52:31 +0300

    update produce dts file

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import dts from "vite-plugin-dts";
 import pkg from "./package.json";
 
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [
+    react(),
+    dts({ rollupTypes: true, tsconfigPath: "./tsconfig.json" }),
+  ],
   server: {
     open: true, // Opens browser automatically
   },
@@ -13,6 +16,7 @@ export default defineConfig({
       entry: "src/index.ts",
       name: "ochom-react-components",
       fileName: "index",
+      formats: ["es", "cjs"],
     },
     rollupOptions: {
       external: [
