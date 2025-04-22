553ab8e94e25164950168ccd77e808957ecb1927
Richard Ochom <ochomrichard752@gmail.com>
2025-04-22 15:03:13 +0300

    update formdata type

--- src/components/Form/fields/select.tsx
+++ src/components/Form/fields/select.tsx
@@ -6,16 +6,30 @@ import { FormField, SelectOption } from "../properties";
 import Loading from "./loading";
 
 const SearchField = ({ field }: { field: FormField }) => {
-  const selected = useMemo(() => {
-    return field.options?.find((opt) => opt.value === field.value) ?? null;
-  }, [field.value, field.options]);
-
   const handleChange = (_e: any, newValue: SelectOption | null) => {
     field.onChange &&
       field.onChange({
         target: { name: field.name, value: newValue?.value ?? "" },
       });
   };
+
+  // remove duplicate options
+  const cleanOptions = useMemo(() => {
+    if (!field.options || field.options.length === 0) return [];
+
+    const uniqueOptions = new Map<string, SelectOption>();
+    field.options.forEach((option) => {
+      if (!uniqueOptions.has(option.value)) {
+        uniqueOptions.set(option.value, option);
+      }
+    });
+
+    return Array.from(uniqueOptions.values());
+  }, [field.options]);
+
+  const selected = useMemo(() => {
+    return cleanOptions.find((opt) => opt.value === field.value) ?? null;
+  }, [field.value, field.options]);
 
   if (field.loading) return <Loading />;
 
@@ -25,12 +39,12 @@ const SearchField = ({ field }: { field: FormField }) => {
       autoHighlight
       size={field.size}
       value={selected}
-      options={field?.options ?? []}
+      options={cleanOptions}
       onChange={handleChange}
+      getOptionKey={(option) => option.label}
       renderInput={(params) => (
         <TextField
           {...params}
-          key={`${field.name}-input`}
           label={field.label}
           required={field.required && !selected}
         />
@@ -43,9 +57,22 @@ const SearchField = ({ field }: { field: FormField }) => {
 const MultiSelectField = ({ field }: { field: FormField }) => {
   const [values, setValues] = useState<SelectOption[]>([]);
 
+  const cleanOptions = useMemo(() => {
+    if (!field.options || field.options.length === 0) return [];
+
+    const uniqueOptions = new Map<string, SelectOption>();
+    field.options.forEach((option) => {
+      if (!uniqueOptions.has(option.value)) {
+        uniqueOptions.set(option.value, option);
+      }
+    });
+
+    return Array.from(uniqueOptions.values());
+  }, [field.options]);
+
   useEffect(() => {
     const currentValues = [];
-    for (const option of field?.options ?? []) {
+    for (const option of cleanOptions) {
       if (field.value && field.value.includes(option.value)) {
         currentValues.push(option);
       }
@@ -70,15 +97,16 @@ const MultiSelectField = ({ field }: { field: FormField }) => {
       multiple
       size={field.size}
       id={`${field.name}-label`}
-      options={field?.options ?? []}
+      options={cleanOptions}
       disableCloseOnSelect
       value={values}
       getOptionLabel={(option) => option.label}
       isOptionEqualToValue={(prev, next) => prev.value === next.value}
       onChange={(_e, newValues) => setValues(newValues)}
-      renderOption={({ ...more }, option) => {
+      getOptionKey={(option) => option.label}
+      renderOption={({ key, ...more }, option) => {
         return (
-          <li {...more} key={option.value}>
+          <li key={key} {...more}>
             <Checkbox
               icon={<Icon icon="mdi:checkbox-blank-outline" />}
               checkedIcon={<Icon icon="mdi:checkbox-marked" />}
@@ -101,7 +129,6 @@ const MultiSelectField = ({ field }: { field: FormField }) => {
       renderInput={(params) => (
         <TextField
           {...params}
-          key={`${field.name}-input`}
           label={field.label}
           required={field.required && values.length === 0}
           placeholder={field?.placeholder ?? ""}
