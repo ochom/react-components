f58ee867c362a784bb86a2f40127042c5f40a53c
Richard Ochom <ochomrichard752@gmail.com>
2025-12-06 09:44:42 +0300

    export everythin in a table

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 export { default as Form } from "./components/Form";
 export { default as Modal } from "./components/Modal/modal";
 export { default as Stepper } from "./components/Stepper";
-export { default as Table } from "./components/Table";
 export { default as Tabs } from "./components/Tabs";
 
+export * from "./components/Table";
 export * from "./components/Drawer/drawer";
 export * from "./components/EmptyPage";
 export * from "./components/Form";
